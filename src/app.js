b063022be6c4351a7e7570f8d92b4efb0934abd1
khushi667 <khushi.mis03@gmail.com>
2024-09-29 12:13:45 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -2,17 +2,22 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+
+const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN, // Ensure CORS_ORIGIN is correctly defined in your .env
     credentials: true
-}))
+}));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
+// rout import
+import userRouter from './routes/user.router.js'; 
 
-export default app;
+app.use("/api/v1/users", userRouter);
+
+export {app}
